f7d1fd03aeba387c567c6c1f53ab6dd518021bc9
jaegeun7 <162278927+jaegeun7@users.noreply.github.com>
2024-03-08 15:03:46 +0900

    Create kernel.c

---
+++ kernel/kernel.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+void print_minios(char* str);
+
+int main() {
+    print_minios("[MiniOS SSU] Hello, World!");
+
+    char *input;
+
+    while(1) {
+        // readline을 사용하여 입력 받기
+        input = readline("커맨드를 입력하세요(종료:exit) : ");
+
+        if (strcmp(input,"exit") == 0) {
+            break;
+        }
+        // 입력받은 커맨드 실행
+        system(input);
+    }
+
+    // 메모리 해제
+    free(input);
+    print_minios("[MiniOS SSU] MiniOS Shutdown........");
+
+    return(1);
+}
+
+void print_minios(char* str) {
+        printf("%s\n",str);
+}
